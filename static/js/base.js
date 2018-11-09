a9d60bef19b8af7b824465abf41cc7de2786569a
Fraser Dale <fraserdale2@gmail.com>
2018-11-09 02:07:31 +0000

    ui stuff

--- static/js/base.js
+++ static/js/base.js
@@ -1,17 +1,20 @@
-(function() {
+(function () {
   const config = require('../config.json')
-  document.getElementById('server').value =  config.server
+  document.getElementById('server').value = config.server
   document.getElementById('private').value = config.privateChannel
   document.getElementById('public').value = config.publicChannel
   document.getElementById('bot').value = config.botToken
   document.getElementById('quantityCart'.value = config.quantityCart)
-  if(config.deleteAfterReact){
+  if (config.deleteAfterReact) {
     document.getElementById('deleteAfterReact').checked = true
   }
 })();
 const electron = require('electron');
-const {ipcRenderer} = electron;
-function stop(){
+const {
+  ipcRenderer
+} = electron;
+
+function stop() {
   document.getElementById('server').disabled = false
   document.getElementById('private').disabled = false
   document.getElementById('public').disabled = false
@@ -23,44 +26,45 @@ function stop(){
   document.getElementById("icon").src = 'https://pbs.twimg.com/profile_images/999669687112749056/WK1RT5lY_400x400.jpg'
 
 }
-ipcRenderer.on('message',function(start,i){
+ipcRenderer.on('message', function (start, i) {
   console.log('x')
 })
 
-ipcRenderer.on('cartsTotal',function(cartsTotal,cartNum){
+ipcRenderer.on('cartsTotal', function (cartsTotal, cartNum) {
   //alert(i)
   document.getElementById('total').innerHTML = cartNum
 })
-ipcRenderer.on('liveTotal',function(liveTotal,liveNum){
+ipcRenderer.on('liveTotal', function (liveTotal, liveNum) {
   //alert(i)
   document.getElementById('live').innerHTML = liveNum
 })
-ipcRenderer.on('redeemedTotal',function(redeemedTotal,redeemedNum){
+ipcRenderer.on('redeemedTotal', function (redeemedTotal, redeemedNum) {
   //alert(i)
   document.getElementById('redeemed').innerHTML = redeemedNum
 })
-ipcRenderer.on('serverImg',function(serverImg,img){
+ipcRenderer.on('serverImg', function (serverImg, img) {
   //alert(i)
   document.getElementById("icon").src = img
 })
-ipcRenderer.on('serverName',function(serverName,name){
+ipcRenderer.on('serverName', function (serverName, name) {
   //alert(i)
   document.getElementById('name').innerHTML = name
 })
-ipcRenderer.on('serverName',function(serverName,name){
+ipcRenderer.on('serverName', function (serverName, name) {
   //alert(i)
   document.getElementById('name').innerHTML = name
 })
-ipcRenderer.on('botName',function(botName,bname){
+ipcRenderer.on('botName', function (botName, bname) {
   //alert(i)
   document.getElementById('botsname').innerHTML = bname
 })
-ipcRenderer.on('loginError',function(loginError,x){
+ipcRenderer.on('loginError', function (loginError, x) {
   //alert(i)
   alert('Login details incorrect')
   stop()
 })
-function save(){
+
+function save() {
   const server = document.querySelector('#server').value;
   const private = document.querySelector('#private').value;
   const public = document.querySelector('#public').value;
@@ -71,13 +75,23 @@ function save(){
   console.log(config)
   ipcRenderer.send('configSave', config);
 }
-function start(){
+
+function start() {
   //save()
-  document.getElementById('server').disabled  = true
+  document.getElementById('server').disabled = true
   document.getElementById('private').disabled = true
   document.getElementById('public').disabled = true
   document.getElementById('bot').disabled = true
   document.getElementById('quantityCart').disabled = true
   document.getElementById('deleteAfterReact').disabled = true
   ipcRenderer.send('start');
+}
+
+function showSettings() {
+  var x = document.getElementById("settings");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
