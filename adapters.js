738b1c707e29cbe2d8e4c1d323266adf98a55b2b
Fraser Dale <fraserdale2@gmail.com>
2019-06-18 18:32:18 +0100

    sole non splash

--- adapters.js
+++ adapters.js
@@ -8,18 +8,18 @@ module.exports.splashforceCartProperties = (e, cartNum) => {
     const size = e.fields[3]['value'];
     const sku = e.fields[2]['value'];
 
-	return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
+    return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
 }
 
 module.exports.ycoppCartProperties = (e, cartNum) => {
     const email = e.fields[0]['value'];
     const pass = e.fields[1]['value'];
-   	const loginURL = e.url;
+    const loginURL = e.url;
     const img = `http://demandware.edgesuite.net/sits_pod20-adidas/dw/image/v2/aaqx_prd/on/demandware.static/-/;Sites-adidas-products/en_US/dw8b928257/zoom/${sku}_01_standard.jpg`;
     const size = e.title.split(',')[1];
     const sku = e.title.split(',')[0];
 
-	return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
+    return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
 }
 
 module.exports.latchkeyCartProperties = (e, cartNum) => {
@@ -30,7 +30,7 @@ module.exports.latchkeyCartProperties = (e, cartNum) => {
     const size = e.fields[2]['value'];
     const sku = e.fields[1]['value'];
 
-	return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
+    return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
 }
 
 module.exports.balkoCartProperties = (e, cartNum) => {
@@ -41,18 +41,28 @@ module.exports.balkoCartProperties = (e, cartNum) => {
     const size = e.fields[2]['value'];
     const sku = e.fields[4]['value'];
 
-	return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
+    return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
 }
 
 module.exports.soleaioCartProperties = (e, cartNum) => {
-    const email = e.fields[6]['value'];
-    const pass = e.fields[7]['value'];
-    const loginURL = e.description;
-    const img = e.thumbnail.url;
-    const size = e.fields[1]['value'];
-    const sku = e.fields[0]['value'];
+    if (e.fields.length >= 6) {
+        const email = e.fields[6]['value'];
+        const pass = e.fields[7]['value'];
+        const loginURL = e.description;
+        const img = e.thumbnail.url;
+        const size = e.fields[1]['value'];
+        const sku = e.fields[0]['value'];
+    } else {
+        const email = e.fields[2]['value'];
+        const pass = e.fields[3]['value'];
+        const loginURL = e.description;
+        const img = e.thumbnail.url;
+        const size = e.fields[1]['value'];
+        const sku = e.title.split(' ')[0];
+    }
 
-	return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
+
+    return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
 }
 
 module.exports.adisplashCartProperties = (e, cartNum) => {
@@ -62,11 +72,11 @@ module.exports.adisplashCartProperties = (e, cartNum) => {
     const pass = userPass.split(': ')[2];
     const loginURL = e.url;
     const sku = e.fields[0]['value'];
-    const img = 'https://transform.dis.commercecloud.salesforce.com/transform/aagl_prd/on/demandware.static/-/Sites-adidas-products/default/zoom/'+sku+'_00_plp_standard.jpg?sw=276&sh=276&sm=fit&strip=false';
+    const img = 'https://transform.dis.commercecloud.salesforce.com/transform/aagl_prd/on/demandware.static/-/Sites-adidas-products/default/zoom/' + sku + '_00_plp_standard.jpg?sw=276&sh=276&sm=fit&strip=false';
     const size = e.fields[1]['value'];
 
-    
-	return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
+
+    return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
 }
 
 module.exports.phantomCartProperties = (e, cartNum) => {
@@ -77,8 +87,8 @@ module.exports.phantomCartProperties = (e, cartNum) => {
     const img = '';
     const size = e.fields[1]['value'];
     const sku = e.fields[0]['value'];
-    
-	return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
+
+    return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
 }
 
 module.exports.gen5CartProperties = (e, cartNum) => {
@@ -88,8 +98,8 @@ module.exports.gen5CartProperties = (e, cartNum) => {
     const img = e.thumbnail.url;
     const size = e.fields[1]['value'];
     const sku = e.fields[0]['value'];
-    
-	return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
+
+    return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
 }
 
 
@@ -100,8 +110,8 @@ module.exports.nomercyCartProperties = (e, cartNum) => {
     const img = e.thumbnail.url;
     const size = e.fields[1]['value'];
     const sku = e.fields[0]['value'];
-    
-	return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
+
+    return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
 }
 
 module.exports.kodaiCartProperties = (e, cartNum) => {
@@ -111,6 +121,6 @@ module.exports.kodaiCartProperties = (e, cartNum) => {
     const img = e.thumbnail.url;
     const size = e.fields[0]['value'];
     const sku = e.fields[1]['value'];
-    
-	return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
+
+    return new CartProperties(cartNum, email, pass, loginURL, img, size, sku);
 }
